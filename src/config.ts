6e37d88e966de33690a80cde302355049f774502
Kyle Yu <gystre@gmail.com>
2020-11-30 13:08:29 -0800

    support for 3060

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
-import {banner} from './banner';
+import { banner } from "./banner";
 
-import {config as config_} from 'dotenv';
-import path from 'path';
+import { config as config_ } from "dotenv";
+import path from "path";
 
-config_({path: path.resolve(__dirname, '../.env')});
+config_({ path: path.resolve(__dirname, "../.env") });
 
 console.info(
 	banner.render(
 		envOrBoolean(process.env.ASCII_BANNER, false),
-		envOrString(process.env.BANNER_COLOR, '#808080')
+		envOrString(process.env.BANNER_COLOR, "#808080")
 	)
 );
 
@@ -23,9 +23,9 @@ function envOrArray(
 	array?: string[]
 ): string[] {
 	return (environment
-		? environment.includes('\n')
-			? environment.split('\n')
-			: environment.split(',')
+		? environment.includes("\n")
+			? environment.split("\n")
+			: environment.split(",")
 		: array ?? []
 	).map((s) => s.trim());
 }
@@ -40,7 +40,7 @@ function envOrBoolean(
 	environment: string | undefined,
 	boolean?: boolean
 ): boolean {
-	return environment ? environment === 'true' : boolean ?? true;
+	return environment ? environment === "true" : boolean ?? true;
 }
 
 /**
@@ -50,7 +50,7 @@ function envOrBoolean(
  * @param string Default string. If not set, is `''`.
  */
 function envOrString(environment: string | undefined, string?: string): string {
-	return environment ? environment : string ?? '';
+	return environment ? environment : string ?? "";
 }
 
 /**
@@ -168,18 +168,18 @@ const browser = {
 		process.env.PAGE_SLEEP_MAX,
 		5000
 	),
-	open: envOrBoolean(process.env.OPEN_BROWSER)
+	open: envOrBoolean(process.env.OPEN_BROWSER),
 };
 
 const docker = envOrBoolean(process.env.DOCKER);
 
-const logLevel = envOrString(process.env.LOG_LEVEL, 'info');
+const logLevel = envOrString(process.env.LOG_LEVEL, "info");
 
 const notifications = {
-	desktop: process.env.DESKTOP_NOTIFICATIONS === 'true',
+	desktop: process.env.DESKTOP_NOTIFICATIONS === "true",
 	discord: {
 		notifyGroup: envOrArray(process.env.DISCORD_NOTIFY_GROUP),
-		webHookUrl: envOrArray(process.env.DISCORD_WEB_HOOK)
+		webHookUrl: envOrArray(process.env.DISCORD_WEB_HOOK),
 	},
 	email: {
 		password: envOrString(process.env.EMAIL_PASSWORD),
@@ -189,7 +189,7 @@ const notifications = {
 			process.env.EMAIL_TO,
 			envOrString(process.env.EMAIL_USERNAME)
 		),
-		username: envOrString(process.env.EMAIL_USERNAME)
+		username: envOrString(process.env.EMAIL_USERNAME),
 	},
 	mqtt: {
 		broker: envOrString(process.env.MQTT_BROKER_ADDRESS),
@@ -197,12 +197,12 @@ const notifications = {
 		password: envOrString(process.env.MQTT_PASSWORD),
 		port: envOrNumber(process.env.MQTT_BROKER_PORT, 1883),
 		qos: envOrNumber(process.env.MQTT_QOS, 0),
-		topic: envOrString(process.env.MQTT_TOPIC, 'streetmerchant/alert'),
-		username: envOrString(process.env.MQTT_USERNAME)
+		topic: envOrString(process.env.MQTT_TOPIC, "streetmerchant/alert"),
+		username: envOrString(process.env.MQTT_USERNAME),
 	},
 	pagerduty: {
 		integrationKey: envOrString(process.env.PAGERDUTY_INTEGRATION_KEY),
-		severity: envOrString(process.env.PAGERDUTY_SEVERITY, 'info')
+		severity: envOrString(process.env.PAGERDUTY_SEVERITY, "info"),
 	},
 	philips_hue: {
 		accessToken: envOrString(process.env.PHILIPS_HUE_CLOUD_ACCESS_TOKEN),
@@ -214,68 +214,68 @@ const notifications = {
 		lightIds: envOrString(process.env.PHILIPS_HUE_LIGHT_IDS),
 		lightPattern: envOrString(process.env.PHILIPS_HUE_LIGHT_PATTERN),
 		refreshToken: envOrString(process.env.PHILIPS_HUE_CLOUD_REFRESH_TOKEN),
-		remoteApiUsername: envOrString(process.env.PHILIPS_HUE_API_KEY)
+		remoteApiUsername: envOrString(process.env.PHILIPS_HUE_API_KEY),
 	},
 	phone: {
 		availableCarriers: new Map([
-			['att', 'txt.att.net'],
-			['attgo', 'mms.att.net'],
-			['bell', 'txt.bell.ca'],
-			['fido', 'fido.ca'],
-			['google', 'msg.fi.google.com'],
-			['koodo', 'msg.koodomobile.com'],
-			['mint', 'mailmymobile.net'],
-			['rogers', 'pcs.rogers.com'],
-			['sprint', 'messaging.sprintpcs.com'],
-			['telus', 'msg.telus.com'],
-			['tmobile', 'tmomail.net'],
-			['verizon', 'vtext.com'],
-			['virgin', 'vmobl.com'],
-			['virgin-ca', 'vmobile.ca']
+			["att", "txt.att.net"],
+			["attgo", "mms.att.net"],
+			["bell", "txt.bell.ca"],
+			["fido", "fido.ca"],
+			["google", "msg.fi.google.com"],
+			["koodo", "msg.koodomobile.com"],
+			["mint", "mailmymobile.net"],
+			["rogers", "pcs.rogers.com"],
+			["sprint", "messaging.sprintpcs.com"],
+			["telus", "msg.telus.com"],
+			["tmobile", "tmomail.net"],
+			["verizon", "vtext.com"],
+			["virgin", "vmobl.com"],
+			["virgin-ca", "vmobile.ca"],
 		]),
 		carrier: envOrArray(process.env.PHONE_CARRIER),
-		number: envOrArray(process.env.PHONE_NUMBER)
+		number: envOrArray(process.env.PHONE_NUMBER),
 	},
 	playSound: envOrString(process.env.PLAY_SOUND),
 	pushbullet: envOrString(process.env.PUSHBULLET),
 	pushover: {
 		priority: envOrNumber(process.env.PUSHOVER_PRIORITY),
 		token: envOrString(process.env.PUSHOVER_TOKEN),
-		username: envOrString(process.env.PUSHOVER_USER)
+		username: envOrString(process.env.PUSHOVER_USER),
 	},
 	slack: {
 		channel: envOrString(process.env.SLACK_CHANNEL),
-		token: envOrString(process.env.SLACK_TOKEN)
+		token: envOrString(process.env.SLACK_TOKEN),
 	},
 	telegram: {
 		accessToken: envOrString(process.env.TELEGRAM_ACCESS_TOKEN),
-		chatId: envOrArray(process.env.TELEGRAM_CHAT_ID)
+		chatId: envOrArray(process.env.TELEGRAM_CHAT_ID),
 	},
 	twilio: {
 		accountSid: envOrString(process.env.TWILIO_ACCOUNT_SID),
 		authToken: envOrString(process.env.TWILIO_AUTH_TOKEN),
 		from: envOrString(process.env.TWILIO_FROM_NUMBER),
-		to: envOrString(process.env.TWILIO_TO_NUMBER)
+		to: envOrString(process.env.TWILIO_TO_NUMBER),
 	},
 	twitch: {
 		accessToken: envOrString(process.env.TWITCH_ACCESS_TOKEN),
 		channel: envOrString(process.env.TWITCH_CHANNEL),
 		clientId: envOrString(process.env.TWITCH_CLIENT_ID),
 		clientSecret: envOrString(process.env.TWITCH_CLIENT_SECRET),
-		refreshToken: envOrString(process.env.TWITCH_REFRESH_TOKEN)
+		refreshToken: envOrString(process.env.TWITCH_REFRESH_TOKEN),
 	},
 	twitter: {
 		accessTokenKey: envOrString(process.env.TWITTER_ACCESS_TOKEN_KEY),
 		accessTokenSecret: envOrString(process.env.TWITTER_ACCESS_TOKEN_SECRET),
 		consumerKey: envOrString(process.env.TWITTER_CONSUMER_KEY),
 		consumerSecret: envOrString(process.env.TWITTER_CONSUMER_SECRET),
-		tweetTags: envOrString(process.env.TWITTER_TWEET_TAGS)
-	}
+		tweetTags: envOrString(process.env.TWITTER_TWEET_TAGS),
+	},
 };
 
 const nvidia = {
 	addToCardAttempts: envOrNumber(process.env.NVIDIA_ADD_TO_CART_ATTEMPTS, 10),
-	sessionTtl: envOrNumber(process.env.NVIDIA_SESSION_TTL, 60000)
+	sessionTtl: envOrNumber(process.env.NVIDIA_SESSION_TTL, 60000),
 };
 
 const page = {
@@ -284,29 +284,30 @@ const page = {
 	screenshot: envOrBoolean(process.env.SCREENSHOT),
 	timeout: envOrNumber(process.env.PAGE_TIMEOUT, 30000),
 	userAgents: envOrArray(process.env.USER_AGENT, [
-		'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
 	]),
-	width: 1920
+	width: 1920,
 };
 
 const proxy = {
 	address: envOrString(process.env.PROXY_ADDRESS),
 	port: envOrNumber(process.env.PROXY_PORT, 80),
-	protocol: envOrString(process.env.PROXY_PROTOCOL, 'http')
+	protocol: envOrString(process.env.PROXY_PROTOCOL, "http"),
 };
 
 // Check for deprecated configuration values
 if (process.env.MAX_PRICE) {
 	console.warn(
-		'ℹ MAX_PRICE is deprecated, please use MAX_PRICE_SERIES_{{series}}'
+		"ℹ MAX_PRICE is deprecated, please use MAX_PRICE_SERIES_{{series}}"
 	);
 }
 
 const store = {
 	autoAddToCart: envOrBoolean(process.env.AUTO_ADD_TO_CART, true),
-	country: envOrString(process.env.COUNTRY, 'usa'),
+	country: envOrString(process.env.COUNTRY, "usa"),
 	maxPrice: {
 		series: {
+			3060: envOrNumber(process.env.MAX_PRICE_SERIES_3060),
 			3070: envOrNumber(process.env.MAX_PRICE_SERIES_3070),
 			3080: envOrNumber(process.env.MAX_PRICE_SERIES_3080),
 			3090: envOrNumber(process.env.MAX_PRICE_SERIES_3090),
@@ -320,37 +321,38 @@ const store = {
 			sf: envOrNumber(process.env.MAX_PRICE_SERIES_CORSAIR_SF),
 			sonyps5c: envOrNumber(process.env.MAX_PRICE_SERIES_SONYPS5C),
 			sonyps5de: envOrNumber(process.env.MAX_PRICE_SERIES_SONYPS5DE),
-			'test:series': -1,
+			"test:series": -1,
 			xboxss: -1,
-			xboxsx: -1
-		}
+			xboxsx: -1,
+		},
 	},
-	microCenterLocation: envOrArray(process.env.MICROCENTER_LOCATION, ['web']),
+	microCenterLocation: envOrArray(process.env.MICROCENTER_LOCATION, ["web"]),
 	showOnlyBrands: envOrArray(process.env.SHOW_ONLY_BRANDS),
 	showOnlyModels: envOrArray(process.env.SHOW_ONLY_MODELS).map((entry) => {
 		const [name, series] = entry.match(/[^:]+/g) ?? [];
 		return {
 			name: envOrString(name),
-			series: envOrString(series)
+			series: envOrString(series),
 		};
 	}),
 	showOnlySeries: envOrArray(process.env.SHOW_ONLY_SERIES, [
-		'3070',
-		'3080',
-		'3090',
-		'rx6800',
-		'rx6800xt',
-		'rx6900xt',
-		'ryzen5600',
-		'ryzen5800',
-		'ryzen5900',
-		'ryzen5950',
-		'sonyps5c',
-		'sonyps5de',
-		'xboxss',
-		'xboxsx'
+		"3060",
+		"3070",
+		"3080",
+		"3090",
+		"rx6800",
+		"rx6800xt",
+		"rx6900xt",
+		"ryzen5600",
+		"ryzen5800",
+		"ryzen5900",
+		"ryzen5950",
+		"sonyps5c",
+		"sonyps5de",
+		"xboxss",
+		"xboxsx",
 	]),
-	stores: envOrArray(process.env.STORES, ['nvidia']).map((entry) => {
+	stores: envOrArray(process.env.STORES, ["nvidia"]).map((entry) => {
 		const [name, minPageSleep, maxPageSleep] = entry.match(/[^:]+/g) ?? [];
 		return {
 			maxPageSleep: envOrNumberMax(
@@ -363,14 +365,14 @@ const store = {
 				maxPageSleep,
 				browser.minSleep
 			),
-			name: envOrString(name)
+			name: envOrString(name),
 		};
-	})
+	}),
 };
 
 export const defaultStoreData = {
 	maxPageSleep: browser.maxSleep,
-	minPageSleep: browser.minSleep
+	minPageSleep: browser.minSleep,
 };
 
 export const config = {
@@ -381,7 +383,7 @@ export const config = {
 	nvidia,
 	page,
 	proxy,
-	store
+	store,
 };
 
 export function setConfig(newConfig: any) {
